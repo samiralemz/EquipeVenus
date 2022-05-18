d7433824fca7e2d0f59bffa2aa01de3ad00d5b0d
Samiralemz <samira.lemos@unifor.com.br>
2022-05-18 20:54:25 -0300

    mudando porta

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require("http");
 
 http.createServer(function(request,response){
     response.end("<h1>Ola mundo aaaaaa</h1>");
-}).listen(8181);
+}).listen(5000);
 console.log("servidor no ar...");
