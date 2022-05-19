d5b741d3ad484cd4b46c1a10620a22d6db922b25
Samiralemz <samira.lemos@unifor.com.br>
2022-05-18 21:03:02 -0300

    Tornando a porta dinâmica de acordo com o servidor

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require("http");
 
+const port = process.env.PORT || 3000
+
 http.createServer(function(request,response){
     response.end("<h1>Ola mundo aaaaaa</h1>");
-}).listen(5000);
+}).listen(port);
 console.log("servidor no ar...");
